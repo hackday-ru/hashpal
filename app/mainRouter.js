ee649453d8f853a8c549364a57425a7accb91a1f
DANILA MARCHENKOV <sprit3dan@gmail.com>
2016-04-16 14:29:49 +0300

    basic states for application

--- app/mainRouter.js
+++ app/mainRouter.js
@@ -1,8 +1,31 @@
+import './states/search/search.template.html';
+import './states/post/post.template.html';
+import './states/home/home.template.html';
+
 export default function mainRouter($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider
         .state('layout', {
             url: '/',
-            template: '<div ui-view>TEST</div>'
+            template: '<div ui-view>layout</div>',
+            controller: () => {
+                console.log('layout');
+            }
+        })
+        .state('home', {
+            url: '',
+            abstract: true,
+            template: '<div ui-view></div>',
+            controller: () => {
+                console.log('home');
+            }
+        })
+        .state('home.search', {
+            url: '/search',
+            templateUrl: './states/search/search.template.html'
+        })
+        .state('home.post', {
+            url: '/post',
+            templateUrl: './states/post/post.template.html'
         });
 }
